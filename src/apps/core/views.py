223430520e4059910b80687d6528c782e0dee6f0
Md.Samiul Haque <samiulh4@gmail.com>
2023-03-11 23:31:48 +0600

    part 1 completed

--- src/apps/core/views.py
+++ src/apps/core/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def frontpage(request):
     return render(request, 'frontpage.html')
+
+def contact(request):
+    return render(request, 'contact.html')
--- src/saulgadgets/urls.py
+++ src/saulgadgets/urls.py
@@ -17,9 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 
-from apps.core.views import frontpage
+from apps.core.views import frontpage,contact
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
+    path('contact/', contact, name="contact"),
     path('admin/', admin.site.urls),
 ]
