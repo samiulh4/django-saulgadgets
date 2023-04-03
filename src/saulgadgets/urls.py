0a5305baa0b221b12e729a564d675f8735d2e3a0
Md.Sami Ul Haque <samiulh4@gmail.com>
2023-04-04 00:55:47 +0600

    working part 2-18:35

--- src/saulgadgets/urls.py
+++ src/saulgadgets/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 
 
 from apps.core.views import frontpage,contact
+from apps.store.views import product_detail
 
 urlpatterns = [
     path('', frontpage, name="frontpage"),
     path('contact/', contact, name="contact"),
+    path('<slug:slug>/', product_detail, name='product_detail'),
     path('admin/', admin.site.urls),
 ]
